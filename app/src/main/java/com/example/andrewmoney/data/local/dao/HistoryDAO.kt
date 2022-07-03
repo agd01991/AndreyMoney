c3e0fb4fce95ae5fb828fbc75b131cbeeb9f6269
agd01991 <agd01991@gmail.com>
2022-07-04 02:33:57 +0300

    add HistoryDAO

--- app/src/main/java/com/example/andrewmoney/data/local/dao/HistoryDAO.kt
+++ app/src/main/java/com/example/andrewmoney/data/local/dao/HistoryDAO.kt
@@ -3,6 +3,7 @@ package com.example.andrewmoney.data.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.andrewmoney.data.local.model.LocalHistoryModel
 import com.example.andrewmoney.data.local.model.LocalVaultModel
 
@@ -10,4 +11,6 @@ import com.example.andrewmoney.data.local.model.LocalVaultModel
 interface HistoryDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun  pushAllHistory(history: List<LocalHistoryModel>)
+    @Query("SELECT * FROM vault")
+    suspend fun getVaults(): List<LocalVaultModel>
 }
--- app/src/main/java/com/example/andrewmoney/data/repository/AppRepository.kt
+++ app/src/main/java/com/example/andrewmoney/data/repository/AppRepository.kt
@@ -2,18 +2,27 @@ package com.example.andrewmoney.data.repository
 
 import android.content.Context
 import android.net.Network
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.andrewmoney.data.local.LocalDataSource
+import com.example.andrewmoney.data.local.model.LocalVaultModel
 import com.example.andrewmoney.data.remote.service.RemoteService
 import com.example.andrewmoney.data.utils.NetworkUtil
 
 class AppRepository (private val remoteService: RemoteService, private val localService: LocalDataSource, private val context: Context) {
+    private val vaultsLiveData = MutableLiveData<List<LocalVaultModel>>()
+    val vaults: LiveData<List<LocalVaultModel>>
+        get() = vaultsLiveData
     suspend fun getLatestVaults(){
         if (NetworkUtil.isInternetAvailable(context)){
-            val vaults = remoteService.getLatestVaults()
-            val _wait = runCatching { vaults }
+            val vaultsRemote = remoteService.getLatestVaults()
+            val vaultsLocal = localService.vaultDAO().getVaults()
+            val _wait = runCatching { vaultsRemote }
             _wait.onSuccess {
                 val rates : Map<String, Double> = it.rates
-                println(rates)
+                rates.forEach {
+
+                }
             }
         }
 
