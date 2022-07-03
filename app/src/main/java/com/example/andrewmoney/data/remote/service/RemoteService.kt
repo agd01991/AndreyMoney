0362d1b8375810a82096239ad152099a24a5b51e
agd01991 <agd01991@gmail.com>
2022-07-03 22:52:15 +0300

    add model

--- app/src/main/java/com/example/andrewmoney/data/remote/service/RemoteService.kt
+++ app/src/main/java/com/example/andrewmoney/data/remote/service/RemoteService.kt
@@ -1,2 +1,9 @@
 package com.example.andrewmoney.data.remote.service
 
+import com.example.andrewmoney.data.local.model.VaultModel
+import retrofit2.http.GET
+
+interface RemoteService {
+    @GET("latest.js")
+    suspend fun getLatestVaults(): VaultModel
+}
