c3203f8d3a3ac4411498e7f10cfd020681369147
agd01991 <agd01991@gmail.com>
2022-07-04 05:40:04 +0300

    add VaultAdapter

--- app/src/main/java/com/example/andrewmoney/ui/vault/Vault.kt
+++ app/src/main/java/com/example/andrewmoney/ui/vault/Vault.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.andrewmoney.R
+import com.example.andrewmoney.databinding.FragmentVaultBinding
 
 class Vault : Fragment(){
+    private lateinit var binding: FragmentVaultBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_vault, container, false)
+       binding = FragmentVaultBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/andrewmoney/ui/vault/adapter/VaultAdapter.kt
+++ app/src/main/java/com/example/andrewmoney/ui/vault/adapter/VaultAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.andrewmoney.ui.vault.adapter
 
-class VaultAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.andrewmoney.data.local.model.LocalVaultModel
+import com.example.andrewmoney.databinding.VaultItemBinding
+
+class VaultAdapter: RecyclerView.Adapter<VaultAdapter.VaultViewHolder>() {
+    private var vaultList: List<LocalVaultModel> = emptyList()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VaultViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = VaultItemBinding.inflate(inflater, parent, false)
+        return VaultViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: VaultViewHolder, position: Int) {
+        val item = vaultList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return vaultList.size
+    }
+    class VaultViewHolder (var binding: VaultItemBinding): RecyclerView.ViewHolder(binding.root)
+
+
 }
